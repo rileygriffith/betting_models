bb93bd5a5bce3a7fb08aa05a422ae5dcdce132fa
rileygriffith <rileyjgriffith@gmail.com>
2024-02-24 18:31:57 -0500

    add back new data prompt

--- main.py
+++ main.py
@@ -79,8 +79,8 @@ def pull_new_lines():
     pull_lines()
 
 if __name__ == "__main__":
-    # new_data = input("Would you like to pull new data? (y/n) ")
-    # if new_data.lower() == "y":
-    #     pull_new_data()
+    new_data = input("Would you like to pull new data? (y/n) ")
+    if new_data.lower() == "y":
+        pull_new_data()
 
     run_prediction()
