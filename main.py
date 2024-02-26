0bffa30837b541bd52252971052ec7ca8a2540a1
rileygriffith <rileyjgriffith@gmail.com>
2024-02-26 09:55:55 -0500

    update formatting, don't display 1h

--- main.py
+++ main.py
@@ -8,68 +8,35 @@ import sys
 
 def run_prediction(tomorrow=False):
     output = predict(tomorrow)
-    results_1h, results_total = [], []
     away_team = None
-    for key, value in output.items():
-        print(Back.GREEN + f'Predictions for {key}:' + Style.RESET_ALL)
-        print(Back.CYAN + 'First Half' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 50 Games: {round(value["1H_50"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 20 Games: {round(value["1H_20"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 10 Games: {round(value["1H_10"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 5 Games: {round(value["1H_5"], 1)}' + Style.RESET_ALL)
-        results_1h += [round(value["1H_50"], 1), round(value["1H_20"], 1), round(value["1H_10"], 1), round(value["1H_5"], 1)]
-        
-        print(Back.CYAN + 'Total' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 50 Games: {round(value["T_50"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 20 Games: {round(value["T_20"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 10 Games: {round(value["T_10"], 1)}' + Style.RESET_ALL)
-        print(f'\t' + Back.BLACK + f'Trend of last 5 Games: {round(value["T_5"], 1)}' + Style.RESET_ALL)
-        results_total += [round(value["T_50"], 1), round(value["T_20"], 1), round(value["T_10"], 1), round(value["T_5"], 1)]
-
-        # Figure out winners and losers of each category
-        if len(results_1h) >= 8 or len(results_total) >= 8:
-            # Split up arrays
-            away_1h = results_1h[:len(results_1h)//2]
-            away_total = results_total[:len(results_total)//2]
-            home_1h = results_1h[len(results_1h)//2:]
-            home_total = results_total[len(results_total)//2:]
-            away_categories_won, home_categories_won = 0, 0
-            for i in range(len(away_1h)):
-                if away_1h[i] > home_1h[i]:
-                    away_categories_won += 1
-                else:
-                    home_categories_won += 1
-                if away_total[i] > home_total[i]:
-                    away_categories_won += 1
-                else:
-                    home_categories_won += 1
-            results_1h, results_total = [], []
-
-            # Output overall predicted winner
-            if home_categories_won > away_categories_won:
-                print(Back.RED + f"{key} wins in {home_categories_won} out of 8 model predictions" + Style.RESET_ALL)
-                margin = round((sum(home_1h) / len(home_1h)) - (sum(away_1h) / len(away_1h)), 1)
-                print(Back.GREEN + f"Average Margin of Victory First Half: {margin}" + Style.RESET_ALL)
-                margin = round((sum(home_total) / len(home_total)) - (sum(away_total) / len(away_total)), 1)
-                print(Back.GREEN + f"Average Margin of Victory Total: {margin}" + Style.RESET_ALL)
-            elif home_categories_won < away_categories_won:
-                print(Back.RED + f"{away_team} wins in {away_categories_won} out of 8 model predictions" + Style.RESET_ALL)
-                margin = round((sum(away_1h) / len(away_1h)) - (sum(home_1h) / len(home_1h)), 1)
-                print(Back.GREEN + f"Average Margin of Victory First Half: {margin}" + Style.RESET_ALL)
-                margin = round((sum(away_total) / len(away_total)) - (sum(home_total) / len(home_total)), 1)
-                print(Back.GREEN + f"Average Margin of Victory Total: {margin}" + Style.RESET_ALL)
-            elif home_categories_won == away_categories_won:
-                print(Back.GREEN + f"Both teams tie in categories won: {home_categories_won}" + Style.RESET_ALL)
-
-            # Output average totals
-            total = round(sum(home_1h) / len(home_1h), 1) + round(sum(away_1h) / len(away_1h), 1)
-            print(Back.GREEN + f"Average First Half Total: {total}" + Style.RESET_ALL)
-            total = round((sum(home_total) / len(home_total)) + (sum(away_total) / len(away_total)), 1)
-            print(Back.GREEN + f"Average Total: {total}" + Style.RESET_ALL)
-            print("-----------------------------------")
+    for game in zip(*(iter(output.keys()),) * 2):
+        away_team = game[0]
+        home_team = game[1]
+        print(Back.GREEN + f'Predictions for {away_team} @ {home_team}:' + Style.RESET_ALL)
+        home_categories_won, away_categories_won = 0, 0
+        for scope in [50, 20, 10, 5]:
+            index = f"T_{scope}"
+            spread = round(abs(output[away_team][index] - output[home_team][index]), 1)
+            print(f'\t' + Back.CYAN + f'Prediction Based on Last {scope} Games:' + Style.RESET_ALL)
+            if output[away_team][index] > output[home_team][index]:
+                print(f'\t' + Back.RED + f"{away_team} -{spread}" + Back.CYAN + f" wins {round(output[away_team][index], 1)} - {round(output[home_team][index], 1)}" + Style.RESET_ALL)
+                away_categories_won += 1
+            elif output[away_team][index] < output[home_team][index]:
+                print(f'\t' + Back.RED + f"{home_team} -{spread}" + Back.CYAN + f" wins {round(output[home_team][index], 1)} - {round(output[away_team][index], 1)}" + Style.RESET_ALL)
+                home_categories_won += 1
+            else:
+                print(f'\t' + Back.RED + f'Tie' + Style.RESET_ALL)
+            total = output[away_team][index] + output[home_team][index]
+            print(f'\t' + Back.CYAN + f"Total {round(total, 1)}" + Style.RESET_ALL)
             print()
+        if away_categories_won > home_categories_won:
+            print(Back.RED + f"{away_team} wins in {away_categories_won} out of 4 model predictions" + Style.RESET_ALL)
+        elif away_categories_won < home_categories_won:
+            print(Back.RED + f"{home_team} wins in {home_categories_won} out of 4 model predictions" + Style.RESET_ALL)
         else:
-            away_team = key
+            print(Back.GREEN + f"Both teams tie in categories won: {home_categories_won}" + Style.RESET_ALL)
+        print("-----------------------------------")
+        print()
 
 def pull_new_data():
     pull_scoreboard()
