462a8d3267fbff8bccab64852fc90a69eed72247
rileygriffith <rileyjgriffith@gmail.com>
2024-02-28 09:17:30 -0500

    lower sleep time to 1 second

--- team_over_under/src/pull_data.py
+++ team_over_under/src/pull_data.py
@@ -15,7 +15,7 @@ def pull_scoreboard(tomorrow):
         day = date.today() - timedelta(days=days_delta)
         print(f"scanning {day} for game data")
         result = json.loads(ScoreboardV2(game_date=day, day_offset=0, league_id="00").get_response())
-        time.sleep(2) # Sleep to prevent rate limiting
+        time.sleep(1) # Sleep to prevent rate limiting
         headers = pd.DataFrame(result["resultSets"][1]["headers"])
         boxscore_df = pd.DataFrame(result["resultSets"][1]["rowSet"], columns=headers[0])
 
@@ -38,7 +38,7 @@ def pull_four_factors():
     print("Pulling team stats...")
     for last_n_games in [50, 20, 10, 5]:
         result = json.loads(LeagueDashTeamStats(last_n_games=last_n_games, per_mode_detailed="PerGame", measure_type_detailed_defense="Four Factors").get_response())
-        time.sleep(2) # Sleep to prevent rate limiting
+        time.sleep(1) # Sleep to prevent rate limiting
         headers = pd.DataFrame(result["resultSets"][0]["headers"])
         df = pd.DataFrame(result["resultSets"][0]["rowSet"], columns=headers[0])
         filename = f"last_{last_n_games}_four_factors.csv"
@@ -48,7 +48,7 @@ def pull_advanced():
     print("Pulling team stats...")
     for last_n_games in [50, 20, 10, 5]:
         result = json.loads(LeagueDashTeamStats(last_n_games=last_n_games, per_mode_detailed="PerGame", measure_type_detailed_defense="Advanced").get_response())
-        time.sleep(2) # Sleep to prevent rate limiting
+        time.sleep(1) # Sleep to prevent rate limiting
         headers = pd.DataFrame(result["resultSets"][0]["headers"])
         df = pd.DataFrame(result["resultSets"][0]["rowSet"], columns=headers[0])
         filename = f"last_{last_n_games}_advanced.csv"
